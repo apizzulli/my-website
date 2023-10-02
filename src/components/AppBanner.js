98e076b0e77eb387c985706c964b2a85b45487c9
apizzulli <anttt.pizzulli@gmail.com>
2023-10-02 17:36:24 -0400

    style tweaks

--- src/components/AppBanner.js
+++ src/components/AppBanner.js
@@ -8,7 +8,6 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-
 export default function AppBanner(){ 
     const [menuClosed, setMenuClosed] = React.useState(false);
     let menuClosedIcon = 
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,9 +4,10 @@ export default function HomePage(){
     return(
         <div class = 'body' >
             <div class = 'body-text'>
-                Welcome!<br></br>This is the home page for my personal site.<br></br> <br></br>
+                <div style={{ height:'75%'}}></div>
+                <div style={{backgroundColor:'rgba(43, 48, 53, .7)',borderRadius:'3px',paddingTop:'4%',paddingBottom:'4%',paddingLeft:'4%',paddingRight:'4%',fontSize:'14pt'}}>Welcome!<br></br>This is the home page for my personal site.<br></br> <br></br>
                 If viewing on desktop, just click the page on the taskbar that you'd like to visit.<br></br><br></br>
-                If viewing on a mobile device, you can use the tab that appears on the top right side of the taskbar to explore.
+                If viewing on a mobile device, you can use the tab that appears on the top right side of the taskbar to explore.</div>
             </div>
         </div>
     );
