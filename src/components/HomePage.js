8c390677ef0d9ff00d8df364b548920197fb91ae
apizzulli <anttt.pizzulli@gmail.com>
2023-10-10 15:45:11 -0400

    small style changes

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,7 +5,7 @@ export default function HomePage(){
         <div class = 'body' >
             <div class = 'body-text'>
                 <div style={{ height:'75%'}}></div>
-                <div style={{backgroundColor:'rgba(43, 48, 53, .7)',borderRadius:'3px',paddingTop:'4%',paddingBottom:'4%',paddingLeft:'4%',paddingRight:'4%',fontSize:'14pt'}}>Welcome!<br></br>This is the home page for my personal site.<br></br> <br></br>
+                <div style={{backgroundColor:'rgba(43, 48, 53, .7)',borderRadius:'3px',paddingTop:'4%',paddingBottom:'4%',paddingLeft:'4%',paddingRight:'4%',fontSize:'14pt'}}>Welcome!<br></br>I'm Anthony Pizzulli, & this is the home page for my personal site.<br></br> <br></br>
                 If viewing on desktop, just click the page on the taskbar that you'd like to visit.<br></br><br></br>
                 If viewing on a mobile device, you can use the tab that appears on the top right side of the taskbar to explore.</div>
             </div>
