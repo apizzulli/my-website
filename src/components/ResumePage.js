0a52392f9f2a90f3fcb8b06b55a1eabb4ba5bfa2
Anthony Pizzulli <anthony.pizzulli@teachingstrategies.com>
2024-06-18 23:07:26 -0400

    pdf view looks good

--- src/components/ResumePage.js
+++ src/components/ResumePage.js
@@ -8,9 +8,9 @@ import ToggleButton from 'react-bootstrap/ToggleButton';
 import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup';
 export default function ResumePage(){
   return (
-    <div style={{backgroundColor:'#2B3035', height: '100vh',color:'white'}}>
+    <div style={{backgroundColor:'#2B3035', height: '100vh', color:'white'}}>
         <div class='title'>You can view my resume embedded within the webpage below.</div><br></br>
-        <iframe type='application/pdf' style={{marginTop:'5%'}}src='./AnthonyPizzulliResume-6.24.pdf' href='./AnthonyPizzulliResume-6.24.pdf' width = '90%' height = '100%'></iframe>
+        <iframe type='application/pdf' style={{ leftMargin: 'auto', rightMargin: 'auto', width:'80%'}}src='./AnthonyPizzulliResume-6.24.pdf' width = '90%' height = '100%'></iframe>
     </div>
   );
 }
