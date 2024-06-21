aac3062415276eba3a7c919f31e4bee1b8c699e5
Anthony Pizzulli <anthony.pizzulli@teachingstrategies.com>
2024-06-21 19:38:07 -0400

    worked on responsiveness

--- src/components/AboutMePage.js
+++ src/components/AboutMePage.js
@@ -20,26 +20,20 @@ export default function AboutMePage(){
             </div>
             <div className="intro-flexbox">
                 <img class='headshot' src = {headshot}></img>
-                <br></br>
-                <p style={{fontSize:'15pt'}}>My name is Anthony and I'm currently a Software Development Engineer
-                    at Publicis Sapient. <br></br><br></br>
-                    I've been studying computer science and programming for around 7 years now, starting in my junior year of high school.
+                    <div><h2>My name is Anthony and I'm currently a Software Development Engineer
+                    at Publicis Sapient. </h2><br></br><br></br>
+                    <p style={{fontSize:'13pt'}}>I've been studying computer science and programming for around 7 years now, starting in my junior year of high school.
                     I noticed right away that I had a passion for it, beginning with Object Oriented programming in Java. <br></br><br></br>
                     I am always open to new opportunities and connections and would love to hear from you! I can be reached at my email or LinkedIn (linked above). I hope you enjoyed learning a little more about me.
-
-                </p>
-            </div>
-            <div style={{marginLeft: 'auto',marginRight: 'auto',fontSize: '16pt',marginTop: '3%',width:'65vw'}}>
-                  
-            </div>
-            <div style={{fontSize: '12pt',marginTop: '3%',width:'65vw', marginLeft: 'auto', marginRight: 'auto'}}>
+                    Check out some of my photos on my other page! 
+                </p></div>
             </div>
             <div className='footer-flexbox'>
                 <div style={{borderRadius: '25px',marginTop:'1%'}}>
                     <img class = 'sbu-image' src={sbu}></img>
                 </div>
                 <div>
-                <img style={{marginLeft: '2%',width:'250px', height:'125px'}}src={ps}></img>
+                <img style={{marginLeft: '2%',width:'150px', height:'75px'}}src={ps}></img>
                 </div>
                 <div></div>
             </div>
